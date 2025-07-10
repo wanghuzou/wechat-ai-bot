e15a2abbfb29b32206837ccee79d039080e32f26
wanghuzou <wanghuzou@outlook.com>
2025-07-10 11:08:50 +0400

    Update and rename wechat.mjs to wx.js

---
+++ api/wx.js
@@ -0,0 +1,48 @@
+const crypto = require("crypto");
+const xml2js = require("xml-js");
+
+module.exports = async (req, res) => {
+  const TOKEN = "fzwltest";
+
+  if (req.method === "GET") {
+    const { signature, timestamp, nonce, echostr } = req.query;
+    const tmpArr = [TOKEN, timestamp, nonce].sort();
+    const tmpStr = crypto.createHash("sha1").update(tmpArr.join("")).digest("hex");
+
+    if (tmpStr === signature) {
+      return res.status(200).send(echostr);
+    } else {
+      return res.status(401).send("Invalid signature");
+    }
+  }
+
+  if (req.method === "POST") {
+    let xmlData = "";
+    req.on("data", chunk => {
+      xmlData += chunk;
+    });
+
+    req.on("end", () => {
+      const json = xml2js.xml2js(xmlData, { compact: true });
+
+      const fromUser = json.xml.FromUserName._cdata;
+      const toUser = json.xml.ToUserName._cdata;
+      const content = json.xml.Content._cdata;
+
+      console.log("收到微信消息：", content);
+
+      const reply = `<xml>
+        <ToUserName><![CDATA[${fromUser}]]></ToUserName>
+        <FromUserName><![CDATA[${toUser}]]></FromUserName>
+        <CreateTime>${Date.now()}</CreateTime>
+        <MsgType><![CDATA[text]]></MsgType>
+        <Content><![CDATA[你说的是：“${content}”]]></Content>
+      </xml>`;
+
+      res.setHeader("Content-Type", "application/xml");
+      res.status(200).send(reply);
+    });
+  } else {
+    res.status(405).send("Method Not Allowed");
+  }
+};
